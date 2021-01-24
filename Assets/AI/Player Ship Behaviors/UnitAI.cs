467acc7f06cd10088f2f286baeac7de5a21c2c86
David Smith <davidcsmith12@gmail.com>
2021-01-24 01:21:11 -0500

    AI attack behavior

--- Assets/AI/Player Ship Behaviors/UnitAI.cs
+++ Assets/AI/Player Ship Behaviors/UnitAI.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class UnitAI : MonoBehaviour
 {
@@ -13,28 +14,42 @@ public class UnitAI : MonoBehaviour
         away,
         to,
         idle,
+
     }
     private State state;
     // States for Combat
-    private enum Combat
+    private enum PlanetCombat
     {
         defense,
         attack
     }
     [SerializeField]
-    private Combat combat = Combat.defense;
+    private PlanetCombat combat = PlanetCombat.defense;
 
+    private enum CombatState
+    {
+        idle,
+        firing,
+    }
+    private CombatState attacking = CombatState.idle;
     private float orbitalLayer;
+    [SerializeField]
+    private float attackDistance = 15f;
+    private GameObject enemy;
+    [SerializeField]
+    private float scanDelay = 3f;
+    private float timeOfScan;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeOfScan = Time.time;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (combat == Combat.attack)
+        if (combat == PlanetCombat.attack)
         {
             orbitalLayer = 7f;
         }
@@ -47,7 +62,7 @@ public class UnitAI : MonoBehaviour
         if (diffMag >= orbitalLayer && diffMag < orbitalLayer + 0.3f)
         {
             OrbitPlanet();
-            if (state != State.idle)
+            if (state != State.idle && attacking != CombatState.firing)
             {
                 if(planetNode.GetComponent<PlanetController>().state == PlanetController.State.empty)
                 {
@@ -68,6 +83,12 @@ public class UnitAI : MonoBehaviour
             MoveToOrbit();
         }
 
+        // Attack if able
+        if(attacking == CombatState.firing)
+        {
+            AttackEnemy();
+        }
+
     }
 
     // Behaviors
@@ -77,7 +98,7 @@ public class UnitAI : MonoBehaviour
     {
         Vector3 diff = (planetNode.transform.position - gameObject.transform.position);
         float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
-        if(combat == Combat.defense)
+        if(combat == PlanetCombat.defense)
         {
             gameObject.transform.rotation = Quaternion.Euler(0f, 0f, rot_z);
         }
@@ -108,6 +129,43 @@ public class UnitAI : MonoBehaviour
     }
     // Attack Nearby Enemies in Sight
 
-    // Change Planets
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("enemy") && Mathf.Abs(timeOfScan - Time.time) >= scanDelay && attacking != CombatState.firing)
+        {
+            // Attack Enemy If Close Enough
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("enemy");
+            //get 3 closest characters (to referencePos)
+            var target = enemies.OrderBy(t => (t.transform.position - transform.position).sqrMagnitude).FirstOrDefault();
+            if (Mathf.Abs(transform.position.magnitude - target.transform.position.magnitude) <= attackDistance)
+            {
+                attacking = CombatState.firing;
+            }
+            timeOfScan = Time.time;
+        }
+       
+    }
+    void AttackEnemy()
+    {
+        // Attack Enemy If Close Enough
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("enemy");
+        //get closest characters (to referencePos)
+        var target = enemies.OrderBy(t => (t.transform.position - transform.position).sqrMagnitude).FirstOrDefault();
+
+        if (target && Mathf.Abs(transform.position.magnitude - target.transform.position.magnitude) <= attackDistance)
+        {
+            // Attack 
+            Vector2 dir = target.transform.position - transform.position;
+            Debug.Log(dir.magnitude);
+            Debug.Log(attacking);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, dir.magnitude);
+            Debug.DrawLine(transform.position, target.transform.position, Color.red);
+            return;
+        }
+        else
+        {
+            attacking = CombatState.idle;
+        }
+    }
 
 }
